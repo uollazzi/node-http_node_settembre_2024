b64da766f56dc8d3f2a958a4db8e11dace3471dd
Matteo Valenzi <dilloawallace@hotmail.com>
2024-09-16 20:53:22 +0200

    welcome

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { createServer, IncomingMessage, ServerResponse } from "node:http";
 import url from "node:url";
+import fs from "node:fs";
+import path from "node:path";
 
 const port = 3000;
 const host = "localhost";
@@ -8,16 +10,42 @@ const requestListener = (req: IncomingMessage, res: ServerResponse) => {
     console.log("Richiesta:", req.method, req.url, new Date().toLocaleString());
 
     const u = url.parse(req.url!, true);
+    const nome = u.query["nome"] as string;
+    const colore = u.query["colore"] as string;
 
     switch (u.pathname) {
         case "/":
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("<html><head><title>Mio Server</title></head><body><h1>Ciao Mondo!</h1></body></html>");
+            res.write("<html><head><title>Mio Server</title></head><body><h1>Ciao [[nome]]!</h1></body></html>".replace("[[nome]]", nome ?? "a tutti"));
             res.end();
             break;
+        case "/api/v2/pippo":
+            res.writeHead(200, { "Content-Type": "application/json" }); // MIME Type
+            const pippo = {
+                nome: "Pippo",
+                anni: 87
+            }
+            res.end(JSON.stringify(pippo));
+            break;
+        case "/benvenuto":
+            let html = fs.readFileSync(path.join("./templates", "welcome.html"), "utf8");
+            res.writeHead(200, { "Content-Type": "text/html" });
+            html = html.replace("{{nome}}", nome ?? "a tutti");
+            html = html.replace("{{colore}}", colore ?? "DFFA8D");
+            res.end(html);
+            break;
         default:
-            res.writeHead(404);
-            res.end("Pagina non trovata.");
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(`
+                <html>
+                    <head>
+                        <title>Mio Server</title>
+                    </head>
+                    <body>
+                        <h1>Pagina non trovata</h1>
+                        <a href="/">Torna alla home</a>
+                    </body>
+                </html>`);
             break;
     }
 };
